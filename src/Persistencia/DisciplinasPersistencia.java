c0568192ffa50ac9c6aaab577da1b9c527b9053b
John Stefan <john.stefan@escolarmanager.com.br>
2020-05-20 13:44:16 -0300

    M0003 - Inserção do Select na Persistencia

--- src/Persistencia/DisciplinasPersistencia.java
+++ src/Persistencia/DisciplinasPersistencia.java
@@ -9,7 +9,11 @@ import Model.DisciplinasModel;
 import Util.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -23,7 +27,7 @@ public class DisciplinasPersistencia {
         conexao = Conexao.getConexao();
     }
     
-    private void InserirDisciplinas(DisciplinasModel disciplinasModel){        
+    public void InserirDisciplinas(DisciplinasModel disciplinasModel){        
         try {
             
             PreparedStatement preparedStatement = conexao.prepareStatement("insert into disciplina (descricao) values (?)");
@@ -35,14 +39,52 @@ public class DisciplinasPersistencia {
         }        
     }
     
-    private void ExcluirDisciplinas(DisciplinasModel disciplinasModel){
+    public void ExcluirDisciplinas(int id){
         try{
             
-            PreparedStatement preparedStatement = conexao.prepareStatement("");
-            //Implementar
+            PreparedStatement preparedStatement = conexao.prepareStatement("delete from displina where id=?");    
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
             
         } catch (SQLException e){
             e.printStackTrace();
         }
+    }
+    
+    public void AtualizarDisciplinas(DisciplinasModel disciplinasModel){
+        try {
+            
+            PreparedStatement preparedStatement = conexao.prepareStatement("update disciplina set descricao=? where id=?");
+            preparedStatement.setString(1, disciplinasModel.getDescricao());
+            preparedStatement.setInt(2, disciplinasModel.getId());
+            preparedStatement.executeUpdate();
+            
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public List<DisciplinasModel> ObterTodasDisciplinas(){
+        List<DisciplinasModel> disciplinas = new ArrayList<DisciplinasModel>();
+        
+        try{
+            
+            Statement statement = conexao.createStatement();
+            ResultSet rs = statement.executeQuery("select * from disciplina");
+            
+            while (rs.next()){
+                DisciplinasModel disciplinasModel = new DisciplinasModel();
+                
+                disciplinasModel.setId(rs.getInt("id"));
+                disciplinasModel.setDescricao("descricao");
+                
+                disciplinas.add(disciplinasModel);
+            }
+            
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        
+        return disciplinas;
     }
 }
